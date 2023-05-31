c9b3801c7243a613d92efba35e7a47acff7d1e53
Nicolás Peña Mogollón <nicop4643@gmail.com>
2023-05-31 18:10:29 -0500

    End of the challenge
    Configurate the page to be responsive, creation of the copy function, changes on variable names to english languaje

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const textArea = document.querySelector(".input-padron");
-const botEncriptar = document.querySelector(".encriptar");
-const botDesencriptar = document.querySelector(".desencriptar");
 const display = document.querySelector(".display");
 const format =  [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
 
-function validarTexto() {
-    let texto = textArea.value;
-    let validador = /[áéíóúÁÉÍÓÚñÑ!@#$%^&*()A-Z]/.test(texto);
-    if (validador){
+function validateText() {
+    let text = textArea.value;
+    let validator = /[áéíóúÁÉÍÓÚñÑ!@#$%^&*()A-Z]/.test(text);
+    if (text == ""){
+        return true;
+    } else if (validator){
         alert("Solo son permitidas letras minúsculas y sin acentos")
         cleanFields();
         return true;
@@ -20,28 +20,44 @@ function cleanFields() {
 
 function encript() {
     let resultText = textArea.value;
-    if (!validarTexto()){
+    if (!validateText()){
         for (let i= 0; i < format.length; i++)
             resultText = resultText.replaceAll(format[i][0], format[i][1]);
+        changeDisplayContent(resultText);
     }
     textArea.value = "";
-    changeDisplayContent(resultText);
 }
 
 function decript(){
     let resultText = textArea.value;
-    if (!validarTexto()){
+    if (!validateText()){
         for (let i= 0; i < format.length; i++)
             resultText = resultText.replaceAll(format[i][1], format[i][0]);
+        changeDisplayContent(resultText);
     }
     textArea.value = "";
-    changeDisplayContent(resultText);
 }
 
 function changeDisplayContent(content){
     display.removeChild(document.querySelector(".display-text"));
     let newDisplayText = document.createElement("textarea");
     newDisplayText.className = "display-text";
+    newDisplayText.readOnly = true;
     newDisplayText.textContent = content;
     display.insertBefore(newDisplayText, document.querySelector(".copy"));
+}
+
+function copyMouseDown() {
+    var textArea = document.querySelector(".display-text");
+    if (textArea.tagName == "TEXTAREA"){
+        textArea.select();
+        document.execCommand("copy");
+    }
+    let copyButton = document.querySelector(".copy");
+    copyButton.textContent = "Copiado!";
+}
+
+function copyMouseUp() {
+    let copyButton = document.querySelector(".copy");
+    copyButton.textContent = "Copiar";
 }
